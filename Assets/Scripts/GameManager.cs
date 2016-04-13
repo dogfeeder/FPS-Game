08d6e02a68d2135dabda76a4bf1316f98361360f
Patrick Freeman <trick05@bigpond.net.au>
2016-04-13 18:03:54 +1000

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public float mouseSensitivity = 5.0f;
+
+    Vector3 pos1;
+    Player player;
+
+
+    public GameObject enemy1;
+
+	public bool yInvert = false;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos1 = new Vector3(Random.Range(-50, 25), 0, Random.Range(0, 100));
+
+        Instantiate(enemy1, pos1, enemy1.transform.rotation);
+	}
+}
---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public GameObject focusPoint;
+    public Slider healthSlider;
+    public float health = 100.0f;
+
+	public int currentAmmo = 30;
+
+	// Use this for initialization
+	void Start () {
+        healthSlider.value = health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (currentAmmo);
+	}
+
+    void takeDamage(float dmg) {
+        health -= dmg;
+        healthSlider.value = health;
+
+        if (health <= 0)
+        {
+            SceneManager.LoadScene("fps base");
+        }
+    }
+}
